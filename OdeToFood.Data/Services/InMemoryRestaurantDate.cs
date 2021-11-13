d51079116b25b4a619aa9367cbbc68aa5384774b
John Kehinde <kehindejohn018@gmail.com>
2021-11-13 06:16:58 +0100

    Created a new ASP.NET MVC application

---
+++ OdeToFood.Data/Services/InMemoryRestaurantDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantDate : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantDate()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Scott's Pizza",
+                    Cuisine = CuisineType.Italian
+                },
+                 new Restaurant
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Tersiguels",
+                    Cuisine = CuisineType.French
+                },
+                  new Restaurant
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Mango Grove",
+                    Cuisine = CuisineType.Italian
+                }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
