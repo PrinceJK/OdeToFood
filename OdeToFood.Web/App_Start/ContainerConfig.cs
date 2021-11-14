451919ba74544975a87780c23a911cd2a978ce22
John Kehinde <kehindejohn018@gmail.com>
2021-11-14 16:52:12 +0100

    clear usings

--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -2,12 +2,8 @@
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 using OdeToFood.Data.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
+using System.Web.Mvc; 
 
 namespace OdeToFood.Web
 {
