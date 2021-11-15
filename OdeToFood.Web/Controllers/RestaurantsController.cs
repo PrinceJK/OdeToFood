40ede989e9e951cb3898b959480058310045d18e
John Kehinde <kehindejohn018@gmail.com>
2021-11-15 09:24:15 +0100

    added an index view to view the list of restaurants

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +9,18 @@ using System.Web.Mvc;
 namespace OdeToFood.Web.Controllers
 {
     public class RestaurantsController : Controller
-    {
+{
+        private readonly IRestaurantData _db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            _db = db;
+        }
         // GET: Restaurants
         public ActionResult Index()
         {
-            return View();
+            var model = _db.GetAll();
+            return View(model);
         }
     }
 }
